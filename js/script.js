f26c0c65310cbd05a331f4b7114a3a8910524ba2
Ksan8 <keziawineberg@Kezia-Winebergs-MacBook-Pro.local>
2016-10-05 12:18:36 -0700

    Add more kittens + create prototype

--- js/script.js
+++ js/script.js
@@ -1,55 +1,110 @@
-var k1 = document.getElementById('kitten1');
-k1.count = 0;
-k1.clicks = document.getElementById('count1');
-k1.name = "Sylvie";
+var catArray = [
+  "Sylvie",
+  "Maurice",
+  "Aundra",
+  "Phil",
+  "Alice",
+  "Tippy",
+  "Ron",
+  "Strombone",
+  "Terence"
+];
 
-var name1 = document.getElementById('name1');
-name1.innerHTML = "This is " + k1.name;
+var catPhotos = [
+  "img/kitten-1.jpg",
+  "img/kitten-2.jpg",
+  "img/kitten-3.jpg",
+  "img/kitten-4.jpg",
+  "img/kitten-5.jpg",
+  "img/kitten-6.jpg",
+  "img/kitten-7.jpg",
+  "img/kitten-8.jpg",
+  "img/kitten-9.jpg"
+];
 
-var k2 = document.getElementById('kitten2');
-k2.count = 0;
-k2.clicks = document.getElementById('count2');
-k2.name = "Maurice";
+var Kitten = function(catNum) {
+  arrayNum = catNum - 1;
+  // number the kitten
+  this.num = catNum;
+  // create list elem
+  this.name = catArray[arrayNum];
+  console.log(this.name);
+  // add photo as property (hidden)
+  this.photo = catPhotos[arrayNum];
+  console.log(this.photo);
+  // add counter as property (hidden)
+  this.count = 0;
 
-var name2 = document.getElementById('name2');
-name2.innerHTML = "This is " + k2.name;
+  // add kitten's name to list
+  listKitten(this.name);
 
-var allKittens = document.getElementsByClassName("kitten");
-var kittenCount = allKittens.length;
+  // count number of clicks on image
+  // this.addEventListener('click', countClicks, false);
+};
 
-// var Kitten = function(sprite) {
-//   this.sprite = 'http://placekitten.com/200/200';
-//
-//   // create a new kitten
-//   function addKitten () {
-//     // create a new div element for a kitten
-//     var newKitten = document.createElement("div");
-//     var newContent = createTextNode("This is ");
-//     newKitten.appendChild(newContent); // add the text node to the new div
-//
-//     // add the new element and content into document
-//     var currentDiv = getElementById("div1");
-//     document.body.insertAfter(newKitten, currentDiv);
-//   }
-//
-//   addKitten();
-//
-//   this.addEventListener('click', countClicks, false);
-// };
+// TODO: function to show photo, name, counter
 
-function countClicks () {
-  //the element has been clicked... do stuff here
-  // "use strict";
-  this.count += 1;
-  // var clickCount = this.count;
-  console.log("Click count:", this.count);
-  // return clickCount;
-  this.clicks.innerHTML = "Clicks: " + this.count;
+// instantiate all kitten objects
+var kittenOne = new Kitten(1);
+var kittenTwo = new Kitten(2);
+var kittenThree = new Kitten(3);
+var kittenFour = new Kitten(4);
+var kittenFive = new Kitten(5);
+var kittenSix = new Kitten(6);
+var kittenSeven = new Kitten(7);
+var kittenEight = new Kitten(8);
+var kittenNine = new Kitten(9);
 
+// add cat names to ordered list
+function listKitten (name) {
+  // create & append list item
+  var newItem = document.createElement("li");
+  newItem.textContent = name;
+  var list = document.getElementById("kitty-list");
+  list.appendChild(newItem);
 }
 
-k1.addEventListener('click', countClicks, false);
-k2.addEventListener('click', countClicks, false);
+function selectKitten () {
 
-// var kittenOne = new Kitten();
-// var kittenTwo = new Kitten();
+  // display the kitten in the cat-box
+  showKitten();
+}
+
+function showKitten () {
+  // change cat name
+  var catName = document.getElementById("name1");
+  catName.innerHTML = "This is " + this.name;
+  // change cat photo to selected kitten
+  var catDiv = document.getElementById("cat-img");
+  catDiv.innerHTML = '<img id="kitten1" class="kitten" src="' + this.photo + 'alt="cute kitten">';
+}
+
+// count user clicks on an image
+function countClicks () {
+  // "use strict";
+  this.count += 1;
+  console.log("Click count:", this.count);
+}
+
+// k1.addEventListener('click', countClicks, false);
+// k2.addEventListener('click', countClicks, false);
+
+// var k1 = document.getElementById('kitten1');
+// k1.count = 0;
+
+// k1.name = "Sylvie";
+//
+// var name1 = document.getElementById('name1');
+// name1.innerHTML = "This is " + k1.name;
+//
+// var k2 = document.getElementById('kitten2');
+// k2.count = 0;
+// k2.clicks = document.getElementById('count2');
+// k2.name = "Maurice";
+//
+// var name2 = document.getElementById('name2');
+// name2.innerHTML = "This is " + k2.name;
+//
+// var allKittens = document.getElementsByClassName("kitten");
+// var kittenCount = allKittens.length;
+
